7aa5512015b1e048d5585dc5f7558c5765eb74f3
Emiliano <epotignano@gmail.com>
2014-11-30 15:08:38 -0300

    First commit

---
+++ client/app/admin/admin.js
@@ -0,0 +1,11 @@
+'use strict';
+
+angular.module('angularPolymerArduinoApp')
+  .config(function ($stateProvider) {
+    $stateProvider
+      .state('admin', {
+        url: '/admin',
+        templateUrl: 'app/admin/admin.html',
+        controller: 'AdminCtrl'
+      });
+  });
